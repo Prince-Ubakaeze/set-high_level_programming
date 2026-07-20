8025b330e754d9e00def73a311b3fd73d21114e6
Prince Ubakaeze <ubakaezeprince@gmail.com>
2026-07-20 16:47:19 +0100

    completed task 7

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Elmwood"
+print(word[0:3])
+print(word[5:])
+print(word[1:6])
+#print(f"First 3 letters: {word_first_3}")
+#print(f"Last 2 letters: {word_last_2}")
+#print(f"Middle word: {middle_word}")
